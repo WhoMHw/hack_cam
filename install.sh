7a295dd49b5fc32cb168cabaa80ba68779168d99
WhoMHw <71681090+WhoMHw@users.noreply.github.com>
2020-12-28 09:34:11 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,12 +11,26 @@ pkg install wget -y
 pkg install php -y
 pkg install unzip -y
 
-wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+arch=$(uname -a | grep -o 'arm' | head -n1)
+arch2=$(uname -a | grep -o 'Android' | head -n1)
+if [[ $arch == *'arm'* ]] || [[ $arch2 == *'Android'* ]] ; then
 
-if [[ -e ngrok-stable-linux-amd64.zip ]]; then
-unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+if [[ -e ngrok-stable-linux-arm.zip ]]; then
+unzip ngrok-stable-linux-arm.zip > /dev/null 2>&1
 chmod +x ngrok
-rm -rf ngrok-stable-linux-amd64.zip
+rm -rf ngrok-stable-linux-arm.zip
 else
-printf "\033[32;1m[\033[33;1m!\033[32;1m]\033[37;1mDownload ngrok eror\033[34;1m Anda harus install sendiri......!!!"
+printf "\e[1;93m[!] Download error... Termux, run:\e[0m\e[1;77m pkg install wget\e[0m\n"
 exit 1
+fi
+
+else
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip > /dev/null 2>&1 
+if [[ -e ngrok-stable-linux-386.zip ]]; then
+unzip ngrok-stable-linux-386.zip > /dev/null 2>&1
+chmod +x ngrok
+rm -rf ngrok-stable-linux-386.zip
+else
+printf "\e[1;93m[!] Download error... \e[0m\n"
+exit 1
+fi
